4af27bc802aa8fb8d978903778daba92d38cad51
RDT1337 <113295512+RDT1337@users.noreply.github.com>
2025-11-16 23:26:04 +0100

    Update progress.js

--- docs/JS/progress.js
+++ docs/JS/progress.js
@@ -1,2 +1,14 @@
-//pobranie do zmiennej wszystkich checkboxów z klasą save-check
-const checkboxes = document.querySelectorAll('.save-check');
+//pobranie do zmiennej wszystkich checkboxów z klasą stan_boxa
+const checkboxes = document.querySelectorAll('.stan_boxa');
+
+//Zaznaczenie na stronie stanu jaki został zrobiony
+checkboxes.forEach(cb => {
+  const saved = localStorage.getItem(cb.id);
+  if(saved !== null){
+    cb.checked = saved === 'true';
+  }
+  //zapisanie stanu przy każdej zmianie
+  cb.addEventListener('change', () => {
+    localStorage.setItem(cb.id, cb.checked);
+  });
+});
